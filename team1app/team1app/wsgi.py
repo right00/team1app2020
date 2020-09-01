c3d67d3e15662a18fb61314399ca738f76570167
kota sato <s1f101802825@iniad.org>
2020-09-01 20:10:58 +0900

    heroku

--- team1app/team1app/wsgi.py
+++ team1app/team1app/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team1app.settings")
 
 application = get_wsgi_application()
+
+#追加
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)

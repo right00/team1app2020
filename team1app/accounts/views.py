95b82f9a7cdff6ea937e99b3d88cd6c21185eb79
kota sato <s1f101802825@iniad.org>
2020-09-18 18:02:40 +0900

    signup complete

--- team1app/accounts/views.py
+++ team1app/accounts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import SignUpForm
 
 class SignUpView(generic.CreateView):
-      form_class = UserCreationForm
+      form_class = SignUpForm
       success_url = reverse_lazy('login')
       template_name = 'accounts/signup.html'

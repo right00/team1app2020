f2e0b3286ae5632d472e3bfcec47675babc6eba8
Karen Nakajima <s1F101800610@iniad.org>
2020-09-30 12:16:32 +0900

    taskの追加

--- team1app/blog/web_urls.py
+++ team1app/blog/web_urls.py
@@ -3,7 +3,8 @@ from . import web_views_public as pub
 from . import web_views_private as priv
 
 urlpatterns = [
-    path('', pub.top , name='top'),
-    path('home/',priv.home,name='home')
+    path('', pub.top, name = 'top'),
+    path('home/', priv.home, name = 'home'),
+    path('home/task', priv.task, name = 'task')
 
 ]
--- team1app/blog/web_views_private.py
+++ team1app/blog/web_views_private.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'blog/private/home.html')
+
+def task(request):
+    return render(request, 'blog/private/task.html')
